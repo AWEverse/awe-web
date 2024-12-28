417142f6d618f0976377c71afe939e38db9f20d4
Andrii Volynets <volinec449@gmail.com>
2024-12-28 16:51:29 +0200

    refactor: update pipe function type definitions and enhance documentation with examples

--- src/lib/core/public/decorators/pipe.ts
+++ src/lib/core/public/decorators/pipe.ts
@@ -1,14 +1,23 @@
-type TRFunc<T, R> = (arg: T) => R;
+type Func<T> = (x: T) => T;
 
 /**
  * @description
  * A pipe function that takes a list of functions and returns a new function that
  * takes an initial value and passes it through the list of functions.
  *
- * @param fns A list of functions to pipe
+ * @param fns - A list of functions to pipe
  * @returns A new function that takes an initial value and passes it through the list of functions
+ *
+ * @example
+ * ```typescript
+ * const addOne = (x: number) => x + 1;
+ * const multiplyByTwo = (x: number) => x * 2;
+ * const addOneAndMultiplyByTwo = pipe(addOne, multiplyByTwo);
+ *
+ * console.log(addOneAndMultiplyByTwo(2)); // 6
+ * ```
  */
-export const pipe =
-  <T>(...fns: TRFunc<any, any>[]) =>
-  (x: T): ReturnType<(typeof fns)[number]> =>
+const pipe =
+  <T>(...fns: Array<Func<T>>) =>
+  (x: T): T =>
     fns.reduce((v, f) => f(v), x);
