cf6c966a2ba9659d7b6bfb76230bbe7cefd92701
Andrii Volynets <volinec449@gmail.com>
2025-04-28 11:26:24 +0300

    refactor: Simplify abort signal handling in task scheduling

--- src/lib/core/public/data-structures/UnrolledTaskQueue.ts
+++ src/lib/core/public/data-structures/UnrolledTaskQueue.ts
@@ -1,3 +1,4 @@
+import { queueMicrotask } from "../polyfill";
 import { CircularQueueNode } from "./UnrolledQueue";
 
 export type TaskFunction = () => void;
@@ -17,14 +18,17 @@ export default class UnrolledTaskQueue<T> implements TaskQueue<T> {
   #head: CircularQueueNode<T>;
   #tail: CircularQueueNode<T>;
   #set: Set<T> = new Set();
+  #batchSize: number;
+  #draining = false;
 
   #maxNodeSize: number = 2048;
   #minNodeSize: number = 128;
   #resizeThreshold: number = 0.75;
   #shrinkThreshold: number = 0.25;
 
-  constructor(nodeSize: number = 1024) {
+  constructor(nodeSize: number = 1024, batchSize: number = 50) {
     this.#nodeSize = nodeSize;
+    this.#batchSize = batchSize;
     const node = new CircularQueueNode<T>({ size: this.#nodeSize });
     this.#head = node;
     this.#tail = node;
@@ -61,38 +65,74 @@ export default class UnrolledTaskQueue<T> implements TaskQueue<T> {
   }
 
   drainEach(callback: (task: T) => void): void {
-    const batch: T[] = [];
-    while (this.#length > 0 && batch.length < 50) {
-      const task = this.#tail.dequeue();
-      if (task !== null && this.#set.has(task)) {
-        this.#set.delete(task);
-        this.#length--;
-        batch.push(task);
+    if (this.#draining) return;
+    this.#draining = true;
+
+    const processBatch = () => {
+      const batch: T[] = [];
+      while (this.#length > 0 && batch.length < this.#batchSize) {
+        let task = this.#tail.dequeue();
+
+        while (task !== null && !this.#set.has(task)) {
+          task = this.#tail.dequeue();
+        }
+
+        if (task !== null && this.#set.has(task)) {
+          this.#set.delete(task);
+          this.#length--;
+          batch.push(task);
+        }
+
+        if (this.#tail.isEmpty() && this.#tail.next) {
+          this.#tail = this.#tail.next;
+        }
       }
-      if (this.#tail.isEmpty() && this.#tail.next) {
-        this.#tail = this.#tail.next;
+      if (batch.length > 0) {
+        queueMicrotask(() => {
+          batch.forEach(callback);
+
+          if (this.#length > 0) {
+            processBatch();
+          } else {
+            this.#draining = false;
+          }
+        });
+      } else {
+        this.#draining = false;
       }
-    }
-    if (batch.length > 0) {
-      requestAnimationFrame(() => batch.forEach(callback));
-    }
+    };
+
+    processBatch();
   }
 
   clear(): void {
     this.#set.clear();
     this.#length = 0;
-    const node = new CircularQueueNode<T>({ size: this.#nodeSize });
-    this.#head = node;
-    this.#tail = node;
+    // Clean up refs for GC
+    let node = this.#tail;
+    while (node) {
+      const next = node.next;
+      node.next = null;
+      node = next!;
+    }
+    const newNode = new CircularQueueNode<T>({ size: this.#nodeSize });
+    this.#head = newNode;
+    this.#tail = newNode;
   }
 
   private adjustNodeSize(): void {
     const fillRatio = this.#length / this.#nodeSize;
-    if (fillRatio >= this.#resizeThreshold && this.#nodeSize < this.#maxNodeSize) {
+    if (
+      fillRatio >= this.#resizeThreshold &&
+      this.#nodeSize < this.#maxNodeSize
+    ) {
       if (this.#length > this.#nodeSize * 1.5) {
         this.#nodeSize = Math.min(this.#nodeSize * 2, this.#maxNodeSize);
       }
-    } else if (fillRatio <= this.#shrinkThreshold && this.#nodeSize > this.#minNodeSize) {
+    } else if (
+      fillRatio <= this.#shrinkThreshold &&
+      this.#nodeSize > this.#minNodeSize
+    ) {
       if (this.#length < this.#nodeSize * 0.1) {
         this.#nodeSize = Math.max(this.#nodeSize / 2, this.#minNodeSize);
       }
